38516fe319abf78482fce3d9dee1a81259dbcdc3
Kyler Akers <kylera14@gmail.com>
2023-02-16 11:14:43 -0600

    Using the new VAPI arch now, it's not super safe but it works to an end and resets after a run

--- GeneticVariantsPatchPlugin.cs
+++ GeneticVariantsPatchPlugin.cs
@@ -8,16 +8,17 @@ namespace GeneticVariantsPatch
     [BepInPlugin(ModGuid, ModName, ModVer)]
     [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("com.RicoValdezio.ArtifactOfGenetics", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInDependency("com.Nebby.VarianceAPI", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.Nebby.VAPI", BepInDependency.DependencyFlags.HardDependency)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
     public class GeneticVariantsPatchPlugin : BaseUnityPlugin
     {
-        public const string ModVer = "0.2.0";
+        public const string ModVer = "0.3.0";
         public const string ModName = "GenVarPatch";
         public const string ModGuid = "com.RicoValdezio.GeneticVariantsPatch";
         public static GeneticVariantsPatchPlugin Instance;
         internal static ManualLogSource LogSource;
         public static ConfigEntry<bool> enableChanceTweaking, enableGeneBlocking;
+        public static ConfigEntry<float> maxGeneChanceInfluence;
 
         private void Awake()
         {
@@ -26,6 +27,7 @@ namespace GeneticVariantsPatch
 
             enableChanceTweaking = Config.Bind<bool>(new ConfigDefinition("General Settings", "Enable Variant Chance Tweaking"), true, new ConfigDescription("If true, the patch will allow the fitness algorithm to change the chance of variants spawning", new AcceptableValueList<bool>(true, false)));
             enableGeneBlocking = Config.Bind<bool>(new ConfigDefinition("General Settings", "Enable Variant Gene Blocking"), true, new ConfigDescription("If true, the patch will prevent variants from receiving genetic bonus and from participating in the fitness algorithm", new AcceptableValueList<bool>(true, false)));
+            maxGeneChanceInfluence = Config.Bind<float>(new ConfigDefinition("General Settings", "Maximum Gene Chance Effect"), 0.5f, new ConfigDescription("The max difference percentage (as a decimal) that the algorithm can change the variant spawn rate - 0.5 is ±50%"));
             GeneVariantDriver.RegisterHooks();
         }
     }
